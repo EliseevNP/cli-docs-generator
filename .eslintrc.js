851086b03e883e72b3d1e7e4424f80e6ef217677
Nikita Eliseev <npeliseev@gmail.com>
2019-10-31 19:19:02 +0300

    TODO: fix 'object-curly-newline' rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,9 @@ module.exports = {
   extends: '@bifot/eslint-config',
   rules: {
     'no-console': 'off',
-    'arrow-parens': ['error', 'as-needed'],
-    'arrow-body-style': ['error', 'as-needed'],
-    'object-curly-newline': ['error', 'never'],
     'no-async-promise-executor': 'off',
     'camelcase': 'off',
+    'arrow-parens': ['error', 'as-needed'],
+    'arrow-body-style': ['error', 'as-needed'],
   }
 };
