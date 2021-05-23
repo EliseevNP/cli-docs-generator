a97e6c4c7c75d1722818e467eaa33e41abbbdb81
Nikita Eliseev <npeliseev@greenatom.ru>
2021-05-23 04:21:28 +0300

    add recursive logic

--- src/helpers/buildAvailableCommands.js
+++ src/helpers/buildAvailableCommands.js
@@ -1,9 +1,9 @@
 module.exports = (cliName, availableCommands) => (availableCommands.length
   ? [
     '## Available commands',
-    availableCommands.map(command => `* [${command.name}](#${command.name})`).join('\n'),
+    availableCommands.map(command => `${'  '.repeat(command.depth - 1)}* [${command.name}]('#${command.name.split(' ').join('-')})`).join('\n'),
     ...availableCommands.map(command => [
-      `### ${command.name}`,
+      `###${'#'.repeat(command.depth - 1)} ${command.name}`,
       `\`\`\`sh\n$ ${cliName} ${command.name} --help\n\`\`\``,
       'Help output:',
       `\`\`\`\n${command.helpOutput}\`\`\``,
