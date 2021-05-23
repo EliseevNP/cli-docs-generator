d0f4b55cacb85748fcd23be56769fad511859f96
Nikita Eliseev <npeliseev@greenatom.ru>
2021-05-23 04:33:10 +0300

    remove '

--- src/helpers/buildAvailableCommands.js
+++ src/helpers/buildAvailableCommands.js
@@ -1,7 +1,7 @@
 module.exports = (cliName, availableCommands) => (availableCommands.length
   ? [
     '## Available commands',
-    availableCommands.map(command => `${'  '.repeat(command.depth - 1)}* [${command.name}]('#${command.name.split(' ').join('-')})`).join('\n'),
+    availableCommands.map(command => `${'  '.repeat(command.depth - 1)}* [${command.name}](#${command.name.split(' ').join('-')})`).join('\n'),
     ...availableCommands.map(command => [
       `###${'#'.repeat(command.depth - 1)} ${command.name}`,
       `\`\`\`sh\n$ ${cliName} ${command.name} --help\n\`\`\``,
