522e33bb1a6aef0f38e648cb949faea6d549a208
zalmane <oren.elias@gmail.com>
2021-10-03 10:36:53 +0300

    handle multiline descriptions in yargs

--- src/helpers/getCommands.js
+++ src/helpers/getCommands.js
@@ -1,46 +1,69 @@
-const exec = require('./exec');
+const exec = require("./exec");
 
-const getCommands = async (cliName, cliExe, helpOutput, depth = 1, previousCommand = '') => {
+const getCommands = async (
+  cliName,
+  cliExe,
+  helpOutput,
+  depth = 1,
+  previousCommand = ""
+) => {
   const commandSection = helpOutput
-    .split('\n\n')
-    .find(section => section.startsWith('Commands:'));
+    .split("\n\n")
+    .find((section) => section.startsWith("Commands:"));
 
   const commands = commandSection
     ? commandSection
-      .split('\n')
-      .slice(1)
-      .map(commandString => {
-        const command = commandString
-          .trim()
-          .replace(`${cliName} ${previousCommand}`, '')
-          .trim()
-          .split(' ')[0];
+        .split("\n")
+        .slice(1)
+        // handle multiline command descriptions
+        .filter((commandString) => {
+          return !commandString.startsWith("   ");
+        })
+        .map((commandString) => {
+          const command = commandString
+            .trim()
+            .replace(`${cliName} ${previousCommand}`, "")
+            .trim()
+            .split(" ")[0];
 
-        return previousCommand
-          ? `${previousCommand} ${command}`
-          : command;
-      })
+          return previousCommand ? `${previousCommand} ${command}` : command;
+        })
     : [];
 
-  return await Promise.all(commands
-    .map(command => new Promise(async (resolve, reject) => {
-      try {
-        const { stdout, stderr } = await exec(`${cliExe} ${command} --help`);
+  return await Promise.all(
+    commands.map(
+      (command) =>
+        new Promise(async (resolve, reject) => {
+          try {
+            const { stdout, stderr } = await exec(
+              `${cliExe} ${command} --help`
+            );
 
-        if (stderr) {
-          reject(stderr);
+            if (stderr) {
+              reject(stderr);
 
-          return;
-        }
+              return;
+            }
 
-        const nestedCommands = await getCommands(cliName, cliExe, stdout, depth + 1, command);
+            const nestedCommands = await getCommands(
+              cliName,
+              cliExe,
+              stdout,
+              depth + 1,
+              command
+            );
 
-        resolve([{ name: command, depth, helpOutput: stdout }, nestedCommands]);
-      } catch (err) {
-        console.log('err', err);
-        reject(err);
-      }
-    })));
+            resolve([
+              { name: command, depth, helpOutput: stdout },
+              nestedCommands,
+            ]);
+          } catch (err) {
+            console.log("err", err);
+            reject(err);
+          }
+        })
+    )
+  );
 };
 
 module.exports = getCommands;
